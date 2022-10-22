625ae6f9cbdbf3c1b9ae593c409b9870088c8851
Bayor-black Abdulmatin <111462882+jamaldak@users.noreply.github.com>
2022-10-22 09:00:59 +0000

    ComGuess  and UserGuess Repositoned

--- index.js
+++ index.js
@@ -3,33 +3,34 @@ console.log(`Is time to get  your game on :  ${Username}`);
 
 let point = 0;
 let maxLevel = 10;
-let level=1;
+let level = 1;
 
-for(i=2;i<=maxLevel;i++){
-    comGuess= Math.floor(Math.random()*i)+1;
-    console.log(comGuess)
-    userGuess = parseInt(prompt(`Enter a number between 1 and ${i}`));  
-    console.log(userGuess);
-  if(isNaN(userGuess)){
+for (i = 2; i <= maxLevel; i++) {
+
+  userGuess = parseInt(prompt(`Enter a number between 1 and ${i}`));
+  console.log("User Guess",userGuess);
+  comGuess = Math.floor(Math.random() * i) + 1;
+  console.log("Computer Guess :",comGuess)
+  if (isNaN(userGuess)) {
     console.log("Invalid Data type,Start Again");
     break;
   }
-  else if (userGuess > i ){
+  else if (userGuess > i) {
     console.log("Number your enter is higher than current max range");
     break;
   }
-  else if(comGuess == userGuess && userGuess >=i ){
-    point+=1;
-    level +=1;
-    console.log("Your current point is : ",point);
-    console.log("Your current Level is level : ",level);    
+  else if (comGuess == userGuess) {
+    point += 1;
+    level += 1;
+    console.log("Your current point is : ", point);
+    console.log("Your current Level is level : ", level);
   }
-  else{
+  else {
     console.log("Guessed Wrong");
     break;
   }
 }
-  
+
 // }
 // for(var i=2; i<maxLevel; i++) {
 //   comGuess= Math.floor(Math.random()*i)+1;
