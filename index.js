77ce3c0625a682029497f0684559170311d3e936
Bayor-black Abdulmatin <111462882+jamaldak@users.noreply.github.com>
2022-10-27 00:26:29 +0000

    Last commit

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-let Username = prompt("Enter your username : ");
-console.log(`Is time to get  your game on :  ${Username}`);
-
 let point = 0;
 let maxLevel = 10;
-let level = 1;
-
-for (i = 2; i <= maxLevel; i++) {
-
+let level = 0;
+ let Username = prompt("Enter your username : ");
+  console.log(`Is time to get  your game on :  ${Username}`);
+for (i = 1; i <= maxLevel; i++) {
   userGuess = parseInt(prompt(`Enter a number between 1 and ${i}`));
   console.log("User Guess",userGuess);
   comGuess = Math.floor(Math.random() * i) + 1;
